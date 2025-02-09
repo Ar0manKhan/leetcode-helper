29a5e4971e9b6755b8c289abd60f659c958a3975
Arman Khan <73093255+Ar0manKhan@users.noreply.github.com>
2025-02-09 20:26:26 +0530

    feat: leetcode api & model prompt and build workflow

--- leetcode.go
+++ leetcode.go
@@ -2,30 +2,45 @@ package main
 
 import (
 	"io"
+	"log"
 	"net/http"
-)
 
-// const LeetcodeAPI = "http://localhost:3000/" // for local testing
-const LeetcodeAPI = "https://alfa-leetcode-api.onrender.com/"
+	"github.com/manifoldco/promptui"
+)
 
 func GetDailyQuestion() (DailyQuestionResponse, error) {
 	// get daily question from leetcode
 	// make /dailyQuestion to leetcode api
-	dailyQuestionUrl := LeetcodeAPI + "dailyQuestion"
+	availableUrls := []string{
+		"https://alfa-leetcode-api.onrender.com/",
+		"http://localhost:3000/",
+	}
+	log.Println("Asking for prompt")
+	prompt := promptui.Select{
+		Label: "Select API URL",
+		Items: availableUrls,
+	}
+	_, result, err := prompt.Run()
+	if err != nil {
+		return DailyQuestionResponse{}, err
+	}
+	leetcodeAPIUrl := result
+	dailyQuestionUrl := leetcodeAPIUrl + "dailyQuestion"
+	log.Println("You selected", leetcodeAPIUrl)
 	// make get api call to daily question url
 	req, err := http.NewRequest("GET", dailyQuestionUrl, nil)
 	if err != nil {
-		panic(err)
+		return DailyQuestionResponse{}, err
 	}
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		panic(err)
+		return DailyQuestionResponse{}, err
 	}
 	defer resp.Body.Close()
 	// read response body
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		panic(err)
+		return DailyQuestionResponse{}, err
 	}
 	return ParseDailyQuestion(body)
 }
